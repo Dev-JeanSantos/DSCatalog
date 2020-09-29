4edd1f2d2c29bc4a497b3425892c1e75d22383ea
Jean Santos <jeancsan@gmail.com>
2020-09-29 09:20:00 -0300

    SEED H2

--- back-end/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ back-end/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -15,6 +16,7 @@ public class CategoryService {
 	@Autowired //injeção de dependencias
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		
 		return repository.findAll();
